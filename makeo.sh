e3604bc7ba6b71ee905b1ff4a21817a3bfc3550f
drawfeel <82626662+drawfeel@users.noreply.github.com>
2021-12-22 14:33:07 +0800

    Add files via upload
    reference compile scripts

---
+++ makeo.sh
@@ -0,0 +1,9 @@
+cd vendor/hisilicon/prebuilts; ls | grep -v Android.mk |xargs rm -rf;cd ../../..;
+mkdir -p ../out/compiling;
+unzip -o $1 -d $(pwd)/vendor/hisilicon/prebuilts;
+source build/envsetup.sh;lunch Hi3796CV300_SEC-eng;
+mkdir ../out/$2;
+rm -fr out;
+./device/hisilicon/build/build.sh -o |tee /home/zhy123/out/$2/build_$2.log;
+cp out/target/product/Hi3796CV300/*.zip ../out/$2/;
+rm -fr ../out/compiling;
